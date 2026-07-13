409736b0743aa217871aa706ba71a3403d432c6e
koray arar <arargame@hotmail.com>
2026-07-13 03:01:44 +0300

    feat(credits): add social media buttons to Credits screen

--- Screens/Game/CreditsScreen.cs
+++ Screens/Game/CreditsScreen.cs
@@ -13,6 +13,10 @@ namespace PaintTrek
         DroppingLightSystem droppingLightSystem;
         List<string> keyLabels = new List<string>();
 
+        TextButton xButton;
+        TextButton ytButton;
+        TextButton ttButton;
+
         public CreditsScreen() 
         {
             Initialize();
@@ -33,6 +37,19 @@ namespace PaintTrek
             droppingLightSystem = new DroppingLightSystem();
 
             backButton = new BackButton("Back", this, true);
+
+            // Social Media Buttons - Center Bottom
+            float buttonsY = 620;
+            float startX = (Globals.GameSize.X - 540) / 2; // (1280 - 540) / 2 = 370
+            
+            xButton = new TextButton("X (Twitter)", new Vector2(startX, buttonsY));
+            xButton.SetOwnerScreen(this);
+            
+            ytButton = new TextButton("YouTube", new Vector2(startX + 190, buttonsY));
+            ytButton.SetOwnerScreen(this);
+            
+            ttButton = new TextButton("TikTok", new Vector2(startX + 380, buttonsY));
+            ttButton.SetOwnerScreen(this);
         }
 
         public override void UnloadContent()
@@ -44,6 +61,10 @@ namespace PaintTrek
         {
             base.Update();
             droppingLightSystem.Update();
+
+            if (xButton != null) xButton.Update();
+            if (ytButton != null) ytButton.Update();
+            if (ttButton != null) ttButton.Update();
         }
 
         public override void Draw()
@@ -73,7 +94,16 @@ namespace PaintTrek
             // Draw "    Sahin Meric" in White
             Globals.SpriteBatch.DrawString(Globals.GameFont, "    Sahin Meric", position, Color.White, 0, origin, 1f, SpriteEffects.None, 0);
             
+            // Draw "FOLLOW US :" in Beige
+            Vector2 followUsPos = new Vector2((Globals.GameSize.X - Globals.GameFont.MeasureString("FOLLOW US :").X) / 2, 550);
+            Vector2 originFollow = new Vector2(0, Globals.GameFont.LineSpacing / 2);
+            Globals.SpriteBatch.DrawString(Globals.GameFont, "FOLLOW US :", followUsPos, Color.Beige, 0, originFollow, 1f, SpriteEffects.None, 0);
+
             Globals.SpriteBatch.End();
+
+            if (xButton != null) xButton.Draw();
+            if (ytButton != null) ytButton.Draw();
+            if (ttButton != null) ttButton.Draw();
 
             base.Draw();
         }
@@ -81,6 +111,42 @@ namespace PaintTrek
         public override void HandleInput()
         {
             base.HandleInput();
+
+            if (xButton != null && xButton.IsClicked)
+            {
+#if ANDROID
+                var uri = Android.Net.Uri.Parse("https://x.com/arargamesstudio");
+                var intent = new Android.Content.Intent(Android.Content.Intent.ActionView, uri);
+                intent.AddFlags(Android.Content.ActivityFlags.NewTask);
+                Android.App.Application.Context.StartActivity(intent);
+#else
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("https://x.com/arargamesstudio") { UseShellExecute = true });
+#endif
+            }
+
+            if (ytButton != null && ytButton.IsClicked)
+            {
+#if ANDROID
+                var uri = Android.Net.Uri.Parse("https://www.youtube.com/@koreaaria");
+                var intent = new Android.Content.Intent(Android.Content.Intent.ActionView, uri);
+                intent.AddFlags(Android.Content.ActivityFlags.NewTask);
+                Android.App.Application.Context.StartActivity(intent);
+#else
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("https://www.youtube.com/@koreaaria") { UseShellExecute = true });
+#endif
+            }
+
+            if (ttButton != null && ttButton.IsClicked)
+            {
+#if ANDROID
+                var uri = Android.Net.Uri.Parse("https://www.tiktok.com/@arargamesstudio");
+                var intent = new Android.Content.Intent(Android.Content.Intent.ActionView, uri);
+                intent.AddFlags(Android.Content.ActivityFlags.NewTask);
+                Android.App.Application.Context.StartActivity(intent);
+#else
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("https://www.tiktok.com/@arargamesstudio") { UseShellExecute = true });
+#endif
+            }
         }
 
         public override void ExitScreen()
